12e3484552db36fbdf6e5b2905965f120ae37cde
sep <maraffonskiy@gmail.com>
2020-07-02 19:46:16 +0300

    model task create and relate to user

---
+++ app/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    /**
+     * @var array
+     */
+    protected $fillable = ['name'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
