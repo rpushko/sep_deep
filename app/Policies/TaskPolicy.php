bf4d7724d278839719d90b1197b91764dafbe854
rpushko <maraffonskiy@gmail.com>
2020-07-07 19:54:40 +0300

    policy add for task destroy

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Policies;
+
+use App\Task;
+use App\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    public function destroy(User $user, Task $task){
+        return $user->id===$task->user_id;
+        $this->authorize('destroy', $task);
+    }
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+
+}
