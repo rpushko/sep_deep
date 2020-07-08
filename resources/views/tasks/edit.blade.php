5cc5ccde15c172f8101d2a81ee877f3e78f63685
rpushko@gmail.com <310675Ehf>
2020-07-09 01:24:23 +0300

    add view edit

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -10,15 +10,16 @@
     @include('common.errors')
 
     <!-- Форма новой задачи -->
-        <form action="{{ route('tasks.store') }}" method="POST" class="form-horizontal">
+        <form action="{{ route('tasks.update',$tasks->id) }}" method="POST" class="form-horizontal">
         {{ csrf_field() }}
+        {{method_field('PUT')}}
 
         <!-- Имя задачи -->
             <div class="form-group">
                 <label for="task" class="col-sm-3 control-label">Задача</label>
 
                 <div class="col-sm-6">
-                    <input type="text" name="name" id="task-name" class="form-control">
+                    <input type="text" name="name" id="task-name" class="form-control" value="{{$tasks->name}}">
                 </div>
             </div>
 
@@ -26,7 +27,7 @@
             <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-6">
                     <button type="submit" class="btn btn-default">
-                        <i class="fa fa-plus"></i> Добавить задачу
+                        <i class="fa fa-plus"></i> Сохранить задачу
                     </button>
                 </div>
             </div>
